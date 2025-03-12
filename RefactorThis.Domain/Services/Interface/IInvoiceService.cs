93b1031d8d67e4510d7d8c670071d41684039b41
Jose Emmanuel Yuson <jep.yuson@gmail.com>
2025-03-12 12:06:44 +0800

    Refactor invoice payment processing

---
+++ RefactorThis.Domain/Services/Interface/IInvoiceService.cs
@@ -0,0 +1,19 @@
+﻿using RefactorThis.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorThis.Domain.Services.Interface
+{
+    public interface IInvoiceService
+    {
+        /// <summary>
+        /// Process the payment for the invoice that matches the payment's reference.
+        /// </summary>
+        /// <param name="payment"></param>
+        /// <returns></returns>
+        string ProcessPayment(Payment payment);
+    }
+}
