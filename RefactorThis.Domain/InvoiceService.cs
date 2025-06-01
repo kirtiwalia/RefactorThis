a1a46c1a04b4aaba4f5558b0005ac9ffc43a9d2d
Nam Cap <capmanhnam@gmail.com>
2025-06-01 21:15:34 +1200

    refactor InvoicePaymentProcessorTests.cs

--- RefactorThis.Domain/InvoiceService.cs
+++ RefactorThis.Domain/InvoiceService.cs
@@ -15,74 +15,84 @@ namespace RefactorThis.Domain
 		}
 
 		public string ProcessPayment(Payment payment)
-		{
-			var inv = _invoiceRepository.GetInvoice(payment.Reference);
+        {
+            var inv = _invoiceRepository.GetInvoice(payment.Reference);
 
-			if (inv == null)
-			{
-				throw new InvalidOperationException("There is no invoice matching this payment");
-			}
-			if (inv.Payments == null)
-			{
-				inv.Payments = new List<Payment>();
-			}
-			if (inv.Amount == 0 && !inv.Payments.Any())
-			{
-				return "no payment needed";
-			}
-			if (inv.Amount == 0 && inv.Payments.Any())
-			{
-				throw new InvalidOperationException("The invoice is in an invalid state, it has an amount of 0 and it has payments.");
-			}
-			var totalPaid = inv.Payments.Sum(x => x.Amount);
-			var remaining = inv.Amount - inv.AmountPaid;
+            var message = HandleInvalidPayments(inv);
 
-			if (totalPaid != 0 && inv.Amount == totalPaid)
-			{
-				return "invoice was already fully paid";
-			}
-			if (totalPaid != 0 && payment.Amount > remaining)
-			{
-				return "the payment is greater than the partial amount remaining";
-			}
-			if (totalPaid == 0 && payment.Amount > inv.Amount)
-			{
-				return "the payment is greater than the invoice amount";
-			}
+            if(message !="")
+                return message;
 
-			// Apply the payment
-			ApplyPayment(inv, payment);
+            var totalPaid = inv.Payments.Sum(x => x.Amount);
+            var remaining = inv.Amount - inv.AmountPaid;
 
-			_invoiceRepository.SaveInvoice(inv);
+            if (totalPaid != 0 && inv.Amount == totalPaid)
+            {
+                return "invoice was already fully paid";
+            }
+            if (totalPaid != 0 && payment.Amount > remaining)
+            {
+                return "the payment is greater than the partial amount remaining";
+            }
+            if (totalPaid == 0 && payment.Amount > inv.Amount)
+            {
+                return "the payment is greater than the invoice amount";
+            }
 
-			var isFinal = (remaining == payment.Amount || inv.Amount == payment.Amount);
+            // Apply the payment
+            ApplyPayment(inv, payment);
 
-			if (isFinal)
-			{
-				if (inv.Payments.Count > 1)
-				{
-					return "final partial payment received, invoice is now fully paid";
-				}
-				else
-				{
-					return "invoice is now fully paid";
-				}
-			}
-			else
-			{
+            _invoiceRepository.SaveInvoice(inv);
 
-				if (inv.Payments.Count > 1)
-				{
-					return "another partial payment received, still not fully paid";
-				}
-				else
-				{
-					return "invoice is now partially paid";
-				}
-			}
-		}
+            var isFinal = (remaining == payment.Amount || inv.Amount == payment.Amount);
 
-		private void ApplyPayment(Invoice inv, Payment payment)
+            if (isFinal)
+            {
+                if (inv.Payments.Count > 1)
+                {
+                    return "final partial payment received, invoice is now fully paid";
+                }
+                else
+                {
+                    return "invoice is now fully paid";
+                }
+            }
+            else
+            {
+
+                if (inv.Payments.Count > 1)
+                {
+                    return "another partial payment received, still not fully paid";
+                }
+                else
+                {
+                    return "invoice is now partially paid";
+                }
+            }
+        }
+
+        private static string HandleInvalidPayments(Invoice inv)
+        {
+            if (inv == null)
+            {
+                return "There is no invoice matching this payment";
+            }
+            if (inv.Payments == null)
+            {
+                inv.Payments = new List<Payment>();
+            }
+            if (inv.Amount == 0 && !inv.Payments.Any())
+            {
+                return "no payment needed";
+            }
+            if (inv.Amount == 0 && inv.Payments.Any())
+            {
+                return "The invoice is in an invalid state, it has an amount of 0 and it has payments.";
+            }
+            return "";
+        }
+
+        private void ApplyPayment(Invoice inv, Payment payment)
 		{
 			inv.AmountPaid += payment.Amount;
 			inv.Payments.Add(payment);
