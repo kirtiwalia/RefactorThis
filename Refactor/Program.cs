b717afd03a59ce241625481c171549ad769656c6
Moses Nova <mosesnova@gmail.com>
2025-09-06 11:21:24 +1200

    Updated Program.cs

--- Refactor/Program.cs
+++ Refactor/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Refactor
+﻿using System;
+using RefactorThis.Domain;
+using RefactorThis.Persistence;
+namespace Refactor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            InvoiceService invoiceService = new InvoiceService();    
+            Invoice invoice = new Invoice(new InvoiceRepository())
+            {
+                Amount = 100,
+                AmountPaid = 100,
+                Type = InvoiceType.Standard
+            };
+            Payment payment = new Payment()
+            {
+                Amount = 100,
+                Reference = "Refactor"
+            };  
+            Console.WriteLine("Released Refactored");
         }
     }
 }
